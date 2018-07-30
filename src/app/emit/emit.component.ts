72bf8157b552b29b2b520647c25ccdb8641fecae
Scott Ho <scott21739@gmail.com>
2018-07-30 16:01:15 +0800

    ''

--- src/app/emit/emit.component.ts
+++ src/app/emit/emit.component.ts
@@ -12,7 +12,7 @@ export class EmitComponent implements OnInit {
   socket: any;
   constructor() {
     
-    this.socket = io('https://5000-dot-4207567-dot-devshell.appspot.com');
+    this.socket = io();
   }
 
   ngOnInit() {
